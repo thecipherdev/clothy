297ef4a7f25de71b701e656abb84cdba88fd9867
JCadiente <cadientejomel0@gmail.com>
2026-01-24 15:11:12 +0800

    chore: remove unused variable

--- src/features/products/context/ProductContext.tsx
+++ src/features/products/context/ProductContext.tsx
@@ -3,15 +3,11 @@ import { Category } from '../types';
 
 
 type ProductContextType = {
-  categories: Category[];
-  setCategories: Dispatch<SetStateAction<Category[]>>;
   editingProduct: string | null;
   setEditingProduct: Dispatch<SetStateAction<string | null>>;
 }
 
 const initialValue = {
-  categories: [],
-  setCategories: () => { },
   editingProduct: "",
   setEditingProduct: () => { },
 }
@@ -24,17 +20,12 @@ export const useProductContext = () => useContext(ProductContext)
 export function ProductContextProvider({
   children,
 }: { children: React.ReactNode }) {
-
-  const [categories, setCategories] = useState<Category[]>([])
   const [editingProduct, setEditingProduct] = useState<string | null>(null);
 
   const value = useMemo(() => ({
-    categories,
-    setCategories,
     editingProduct,
     setEditingProduct,
   }), [
-    categories,
     editingProduct,
   ])
 
